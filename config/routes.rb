a76ee8d493c4fe13996ce65671de69fa2423dad1
jfqd <jfqd@blun.org>
2013-08-27 10:01:03 +0200

    add route for using globalize2

--- config/routes.rb
+++ config/routes.rb
@@ -3,17 +3,19 @@ ActionController::Routing::Routes.draw do |map|
     Radiant::Config['tags.results_page_url'] = TagsExtension::DEFAULT_RESULTS_URL if Radiant::Config['tags.results_page_url'].blank?
   end
   begin
+    
     if defined?(SiteLanguage) && SiteLanguage.count > 0
       include Globalize
       SiteLanguage.codes.each do |code|
         langname = Locale.new(code).language.code
         map.connect "#{langname}#{Radiant::Config['tags.results_page_url']}/:tag", :controller => 'site', :action => 'show_page', :url => Radiant::Config['tags.results_page_url'], :language => code
       end
-    else if defined?(VhostExtension)
-      map.connect "#{Radiant::Config['tags.results_page_url']}/:tag", :controller => 'site', :action => 'show_page', :url => Radiant::Config['tags.results_page_url']
+    elsif defined?(Globalize2Extension)
+      map.connect "/:locale#{Radiant::Config['tags.results_page_url']}/:tag/", :controller => 'site', :action => 'show_page', :url => Radiant::Config['tags.results_page_url']
     end
-      map.connect "#{Radiant::Config['tags.results_page_url']}/:tag", :controller => 'site', :action => 'show_page', :url => Radiant::Config['tags.results_page_url']
-    end
+
+    map.connect "#{Radiant::Config['tags.results_page_url']}/:tag", :controller => 'site', :action => 'show_page', :url => Radiant::Config['tags.results_page_url']
+    
   rescue
     # dirty hack; need to get trough here to allow migrations to run..
   end
