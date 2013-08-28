fcd67eb1fae8c7df5f036ec51b9f66f9a166a034
jfqd <jfqd@blun.org>
2013-08-28 22:16:56 +0200

    fox wrong migration order

--- db/migrate/20130828083210_add_locale_to_taggings.rb
+++ db/migrate/20130828083210_add_locale_to_taggings.rb
@@ -1,9 +1,11 @@
 class AddLocaleToTaggings < ActiveRecord::Migration
   def self.up
     add_column     :taggings, :locale,  :string
+    add_index      :taggings,  [:taggable_id, :taggable_type, :locale]
   end
 
   def self.down
-    remove_column  :taggings, :locale,  :string
+    remove_column  :taggings, :locale
+    remove_index   :taggings,  [:taggable_id, :taggable_type, :locale]
   end
 end
