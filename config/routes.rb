9300c05b5467396b2f1e1f6ded3a8f9abb64496e
jfqd <jfqd@blun.org>
2014-06-06 10:49:10 +0200

    localize tag routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,13 @@ ActionController::Routing::Routes.draw do |map|
   begin
     
     if defined?(Globalize2Extension)
-      map.localized_category_search "/:locale/tag/:tag/",
-                                    :controller => 'site',
-                                    :action => 'show_page',
-                                    :url => "/:locale/tag/"
+      map.localized(I18n.available_locales, :verbose => false) do
+        map.localized_tag_search "/:locale/tag/:tag/", :controller => 'site', :action => 'show_page', :url => "/:locale/tag/"
+      end
     end
-    map.category_search "/tag/:tag/",
-                        :controller => 'site',
-                        :action => 'show_page',
-                        :url => "/tag/"
+    map.localized(I18n.available_locales, :verbose => false) do
+      map.tag_search "/tag/:tag/", :controller => 'site', :action => 'show_page', :url => "/tag/"
+    end
     
   rescue
     # dirty hack; need to get trough here to allow migrations to run..
