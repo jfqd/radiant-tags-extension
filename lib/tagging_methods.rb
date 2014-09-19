44fe3c569a1059bd416c4d787b544f882c6b83ce
jfqd <jfqd@blun.org>
2014-09-19 10:00:16 +0200

    make sure we can delete tags from a page: @new_tags is nil on ordering, but empty if no tags assigned to a page

--- lib/tagging_methods.rb
+++ lib/tagging_methods.rb
@@ -9,10 +9,9 @@ TaggingMethods = Proc.new do
     self.save_without_tags!
     
     # just skip the whole method if @new_tags is
-    # empty or the tags string hasn't changed.
-    # @new_tags might be empty or nil while re-
-    # ordering pages!
-    return self if @new_tags.blank? || @new_tags == tag_list
+    # nil or the tags string hasn't changed.
+    # @new_tags will be nil on reordering pages!
+    return self if @new_tags.nil? || @new_tags == tag_list
     
     # tags have changed, so we delete all taggings and re-create to preserve order
     # back hack to only delete the taggings by the current locale
