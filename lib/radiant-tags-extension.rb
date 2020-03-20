fdcbac2108d5530e905218de4bbe5c6a4480c341
jfqd <jfqd@blun.org>
2020-03-20 18:14:40 +0100

    bump version to 1.6.32

--- lib/radiant-tags-extension.rb
+++ lib/radiant-tags-extension.rb
@@ -1,5 +1,5 @@
 module RadiantTagsExtension
-  VERSION = '1.6.31'
+  VERSION = '1.6.32'
   AUTHORS = ['Benny Degezelle']
   EMAIL = ['benny@gorilla-webdesign.be']
   HOMEPAGE = 'https://github.com/jfqd/radiant-tags-extension'
